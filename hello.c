af2bc018c41e04f9b4d369191c61831171111ce1
jmfrane113 <jmfrane@gmail.com>
2019-01-17 14:50:11 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Frane
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack Frane\n");
+
+  return 0;
+}
